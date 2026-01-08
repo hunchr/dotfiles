b86a76cfd7741358d1c6c860b5f043136e78c66b
Chris <76159444+hunchr@users.noreply.github.com>
2026-01-08 17:34:45 +0000

    Update dotfiles

--- setup.sh
+++ setup.sh
@@ -12,13 +12,13 @@ type -p brew > /dev/null 2>&1 || (echo 'error: brew not found' && exit 1)
 # Replace {{VARIABLE}} with value of $VARIABLE
 replace_placeholders() {
   while grep -q '{{[A-Z_]\+}}' "$1"; do
-    var=$(grep -o '{{[A-Z_]\+}}' "$1" | head -1 | tr -d '{}')
-    value=$(eval echo "\$$var")
+    placeholder=$(grep -o '{{[A-Z_]\+}}' "$1" | head -1 | tr -d '{}')
+    value=$(eval echo "\$$placeholder")
 
     if sed --version > /dev/null 2>&1; then
-      sed -i "s|{{$var}}|$value|g" "$1"
+      sed -i "s|{{$placeholder}}|$value|g" "$1"
     else
-      sed -i '' "s|{{$var}}|$value|g" "$1"
+      sed -i '' "s|{{$placeholder}}|$value|g" "$1"
     fi
   done
 }
@@ -31,40 +31,47 @@ plugin() {
 
 # Install brew formulae and casks
 if [ "${1-}" = i ]; then
+  # Output of `brew leaves | tr '\n' ' '`
   brew install --formula act automake awscli bash bazel bombardier cmake \
-    coreutils deno docker dotnet e2fsprogs elixir-ls exiftool fd ffmpeg \
-    findutils fswatch geckodriver gh git gnu-sed gnu-tar go graphviz \
-    heroku/brew/heroku httpd hyfetch hyperfine jpeg jq jwt-cli kind kubectx \
-    libyaml llvm mas mise mkcert mysql ninech/taps/nctl ninja nmap nuget \
-    ollama pdftk-java pgvector pinentry-mac postgresql@18 pygments redis \
-    rustup s3cmd semaphoreci/tap/sem shellcheck tree valkey vips viu wabt wget \
-    xcodegen yarn yq yt-dlp zsh
+    coreutils curl deno docker-completion e2fsprogs elixir-ls exiftool fd \
+    ffmpeg findutils fswatch geckodriver gh git gnu-sed gnu-tar go graphviz \
+    heroku/brew/heroku  httpd hyfetch hyperfine ios-deploy jansson \
+    jorgelbg/tap/pinentry-touchid jpeg jq jwt-cli kind kubectx \
+    libimobiledevice libxslt libyaml llvm mas minio-mc mise mkcert mysql nginx \
+    ninech/taps/nctl ninja nmap ollama pdftk-java pgvector pinentry-mac \
+    postgresql@18 pygments redis ripgrep rustup s3cmd semaphoreci/tap/sem \
+    shellcheck tree vips viu wabt wget xcodegen yarn yq yt-dlp zip zsh
 
   brew install --cask 1password-cli android-studio chromedriver docker-desktop \
     fork git-credential-manager google-chrome handbrake-app libreoffice \
-    librewolf ngrok obs pgadmin4 proton-drive proton-pass protonvpn \
+    librewolf mullvad-vpn ngrok obs pgadmin4 proton-drive proton-pass \
     qbittorrent qlvideo raycast signal slack spotify stolendata-mpv \
-    thunderbird tunnelblick vlc whatsapp
+    the-unarchiver thunderbird tunnelblick vlc whatsapp
 fi
 
 # Copy config files to home directory
-for node in home/*; do
-  cp -fR "$node" "$HOME"
-  [ -d "$node" ] || replace_placeholders "$HOME/$(basename "$node")"
-done
+while IFS= read -rd '' file; do
+  target="${file/home/"$HOME"}" 
+
+  mkdir -p "$(dirname "$target")"
+  echo "copy: $file -> $target"
+  cp "$file" "$target"
+  replace_placeholders "$target"
+done < <(find home -type f -print0)
 
 # Zsh
+mkdir -p "$HOME/.zfunc"
 plugin zdharma-continuum/fast-syntax-highlighting
 plugin marlonrichert/zsh-autocomplete
 
 # Bun
 type -p bun > /dev/null 2>&1 || (curl -fsSL https://bun.sh/install | bash)
-mkdir -p "$HOME/.zfunc"
 ln -fs "$HOME/.bun/_bun" "$HOME/.zfunc"
 
 # mkcert
 cert="$HOME/.localhost-cert.pem"
 key="$HOME/.localhost-key.pem"
+
 if [ ! -f "$cert" ] || [ ! -f "$key" ]; then
   mkcert -install
   mkcert -cert-file "$cert" -key-file "$key" localhost
